ed32ec104b816abb408704ddf7a3381929c6b74b
Francisco Afonso <paco.afonso.perez@gmail.com>
2017-03-02 21:41:45 +0000

    Refactor and some implementation in Create and Read methods

--- index.php
+++ index.php
@@ -2,37 +2,73 @@
 
 interface Database{
         function create($field, $value, $table);
-        function read($data = array(), $table);
-        function update($data = array(), $table);
-        function delete($data = array(), $table);
+        function read($field=null, $value=null, $table);
+        function update($field, $value, $table);
+        function delete($field, $value, $table);
 }
 
 class mySQL implements Database{
-
-        public $ddbb;
-
-        function __construct($host, $user, $pass, $ddbb) {
+        private $db;
+    /**
+     * mySQL constructor.
+     * @param $host
+     * @param $user
+     * @param $pass
+     * @param $ddbb
+     */
+    function __construct($host, $user, $pass, $db) {
           try {
-            $this->ddbb = mysqli_connect($host, $user, $pass, $ddbb);
+            $this->db = mysqli_connect($host, $user, $pass, $db);
           } catch(Exception $e) {
               echo 'Message: ' .$e->getMessage();
             }
         }
 
         function create($field, $value, $table){
-            echo 'INSERT';
+          $sql = "INSERT INTO $table".
+                 "($field) ".
+                 "VALUES ".
+                "('$value')";
+          if($query = mysqli_query($this->db, $sql))
+            echo 'Operation INSERT, OK.';
+          else {
+              echo $this->ddbb->error;
+            }
         }
 
-        function read($data = array(), $table){
-          echo 'SELECT';
+        function read($field=null, $value=null, $table){
+          if (($field == null) && ($value == null)){
+            $sql = "SELECT * FROM $table";
+            if($query = mysqli_query($this->db, $sql)){
+              echo "Operation SELECT, OK <br>";
+              $row = mysqli_fetch_array($query);
+              print_r($row);
+            }else {
+              echo $this->db->error;
+            }
+          }
+
+          if ($value == null){
+            $sql = "SELECT $field FROM $table";
+            if($query = mysqli_query($this->db, $sql)){
+              echo "Operation SELECT, OK <br>";
+              $row = mysqli_fetch_array($query);
+              print_r($row);
+            }else {
+              echo $this->ddbb->error;
+            }
         }
-        function update($data = array(), $table){
+
+        }
+        function update($field, $value, $table){
           echo 'UPDATE';
         }
-        function delete($data = array(), $table){
+        function delete($field, $value, $table){
           echo 'DROP';
         }
 }
 
 $pdo = new mySQL('localhost','root','','darknd');
-$pdo->create('field','value','table');
+//$pdo->create('text','Im a text','data');
+//$pdo->read(null,null,'data');
+$pdo->read('text',null,'data');
